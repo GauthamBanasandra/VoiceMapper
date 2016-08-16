3edc1fdeeb6d4932db95f418418a9d0ea5d82d2c
Gautam B <t-gauba@microsoft.com>
2016-08-16 22:25:28 +0530

    Adding documentation for FeatureVectorBuilder.

--- src/FeatureVector/FeatureVectorBuilder.java
+++ src/FeatureVector/FeatureVectorBuilder.java
@@ -7,9 +7,11 @@ import java.io.IOException;
 
 /**
  * Created by gauth_000 on 16-Aug-16.
+ * Builds the feature vector for a given audio file
  */
 public class FeatureVectorBuilder
 {
+    // Configuration file - contains info about the model to be used etc
     private ConfigurationManager configurationManager;
 
     public FeatureVectorBuilder()
@@ -17,6 +19,12 @@ public class FeatureVectorBuilder
         this.configurationManager=new ConfigurationManager(FeatureVectorBuilder.class.getResource("/resources/frontend.config.xml"));
     }
 
+    /**
+     * @param inputAudioFileName Path to input audio file name
+     * @param outputFileName Path to output file name to dump the feature vector
+     * @param frontEndName Possible values are - cepstraFrontEnd, spectraFrontEnd, plpFrontEnd
+     * @throws IOException
+     */
     public void dump_feature_vector(String inputAudioFileName, String outputFileName, String frontEndName) throws IOException
     {
         FeatureFileDumper featureDumper=new FeatureFileDumper(configurationManager, frontEndName);
