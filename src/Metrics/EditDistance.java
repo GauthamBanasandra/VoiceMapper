d08077ff9515a8dd3beb27202ddefefe7f29b5a9
Gautam B <t-gauba@microsoft.com>
2016-08-30 00:10:44 +0530

    Full commit

---
+++ src/Metrics/EditDistance.java
@@ -0,0 +1,9 @@
+package Metrics;
+
+/**
+ * Created by gauth_000 on 22-Aug-16.
+ */
+public class EditDistance
+{
+
+}
--- src/VoiceMapper.java
+++ src/VoiceMapper.java
@@ -9,9 +9,15 @@ public class VoiceMapper
 {
     public static void main(String[] args) throws IOException
     {
-        FeatureVectorBuilder builder=new FeatureVectorBuilder();
-        builder.dump_feature_vector("C:\\Users\\gauth_000\\Documents\\Projects\\VoiceMapper\\Voice samples\\sample1.m4a",
-                "C:\\Users\\gauth_000\\Documents\\Projects\\VoiceMapper\\Voice samples\\sample1_dump.txt",
+        FeatureVectorBuilder builder = new FeatureVectorBuilder();
+        /*for (int i = 2; i < 7; i++)
+        {
+            builder.dump_feature_vector("C:\\Users\\gauth_000\\Documents\\Projects\\VoiceMapper\\Voice samples\\Sample\\Recording (" + i + ").m4a",
+                    "C:\\Users\\gauth_000\\Documents\\Projects\\VoiceMapper\\Voice samples\\Sample\\Recording (" + i + ").txt",
+                    "cepstraFrontEnd");
+        }*/
+        builder.dump_feature_vector("C:\\Users\\gauth_000\\Documents\\Projects\\VoiceMapper\\Voice samples\\Sample\\Recording.m4a",
+                "C:\\Users\\gauth_000\\Documents\\Projects\\VoiceMapper\\Voice samples\\Sample\\Recording.txt",
                 "cepstraFrontEnd");
     }
 }
